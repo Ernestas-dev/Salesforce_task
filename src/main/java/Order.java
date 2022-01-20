7fa2bf391008c36271f3d1216eb9ba55b29ff8c7
Ernestas Šalna <ernesto.kristy@gmail.com>
2022-01-20 19:34:32 +0200

    Order and OrderItem classes prepared

--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -1,2 +1,12 @@
 public class Order {
+
+    int item_count;
+    int order_id;
+
+    // Order constructor
+    public Order(int item_count, int order_id) {
+        this.item_count = item_count;
+        this.order_id = order_id;
+    }
+
 }
--- src/main/java/OrderItem.java
+++ src/main/java/OrderItem.java
@@ -1,2 +1,28 @@
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+
 public class OrderItem {
+
+    int order_id;
+    int product_id;
+    String provisioning_date;
+
+    // OrderItem constructor
+    public OrderItem(int order_id, int product_id) {
+        this.order_id = order_id;
+        this.provisioning_date = this.get_provisioning_date();
+        this.product_id = product_id;
+    }
+
+    /**
+     * When executed <br>
+     * @return provisioning date
+     */
+
+    public String get_provisioning_date() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+
+        return dtf.format(now);
+    }
 }
