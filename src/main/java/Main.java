3dc584f810ff236003b0dad36e92c7015b3d6700
Ernestas Šalna <ernesto.kristy@gmail.com>
2022-01-20 19:33:31 +0200

    Main class updated to take in prepared data from ReadFile

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,17 @@
+import java.io.IOException;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Test");
+
+    public static void main(String[] args) throws IOException {
+        // Execute orders.txt file
+        ReadFile newFile = new ReadFile();
+        List data = newFile.ReturnList();
+
+        // Execute app
+        AssetManagementTask newAsset = new AssetManagementTask();
+//        newAsset.executeBatch(data);
+
     }
+
 }
